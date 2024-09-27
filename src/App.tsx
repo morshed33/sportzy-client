a8be2bc15d74b863d1c067a45eb92aded1ae145a
morshed33 <ac3m0r@gmail.com>
2024-09-27 20:08:48 +0600

    integrated shadcn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import { Button } from "./components/ui/button";
 
 const App: FC = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen">
         <p className="text-4xl">WELCOME TO THE SPORTZY</p>
+        <Button>Click ME</Button>
       </div>
     </>
   );
